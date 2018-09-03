08859a03f4ffc1edefce209e0f4ee3ef3a5e5393
miguelhasbun <miguelneptaly@gmail.com>
2018-09-03 17:00:34 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double op;
+    cout<<"Ingrese una opcion:"<<endl;
+    cin>>op;
+    while(op!=0){
+
+        cout<<"1. CREAR UNA BASE DE DATOS"<<endl;
+        cout<<"2. BORRAR UNA BASE DE DATOS"<<endl;
+        cout<<"3. CREAR UNA TABLA"<<endl;
+        cout<<"4. ELIMINAR UNA TABLA"<<endl;
+        cout<<"5. INSERTAR UN REGISTRO A LA TABLA"<<endl;
+        cout<<"6. ELIMINAR REGISTROS DE UNA TABLA"<<endl;
+        cout<<"7. ACTUALIZAR REGISTRO DE UNA TABLA"<<endl;
+        cout<<"8. SELECCIONAR REGISTROS DE UNA TABLA"<<endl;
+        if (op==1){
+            cout<<"hola"<<endl;
+            cout<<"Ingrese una opcion"<<endl;
+            cin>>op;
+        }
+        if (op==2){
+            cout<<"hola"<<endl;
+            cout<<"Ingrese una opcion"<<endl;
+            cin>>op;
+        }
+        if (op==3){
+            cout<<"hola"<<endl;
+            cout<<"Ingrese una opcion"<<endl;
+            cin>>op;
+        }
+        if (op==4){
+            cout<<"hola"<<endl;
+            cout<<"Ingrese una opcion"<<endl;
+            cin>>op;
+        }
+        if (op==5){
+            cout<<"hola"<<endl;
+            cout<<"Ingrese una opcion"<<endl;
+            cin>>op;
+        }
+        if (op==6){
+            cout<<"hola"<<endl;
+            cout<<"Ingrese una opcion"<<endl;
+            cin>>op;
+        }
+        if (op==7){
+            cout<<"hola"<<endl;
+            cout<<"Ingrese una opcion"<<endl;
+            cin>>op;
+        }
+        if (op==8){
+            cout<<"hola"<<endl;
+            cout<<"Ingrese una opcion"<<endl;
+            cin>>op;
+        }
+
+
+    }
+    return 0;
+}
+
